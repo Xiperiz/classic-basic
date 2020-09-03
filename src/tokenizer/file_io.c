49ea6470b9ad54797836bfa14d172ada65a06244
Xiperiz <ziperix105@gmail.com>
2020-09-03 13:21:12 +0100

    Account for last line

--- src/tokenizer/file_io.c
+++ src/tokenizer/file_io.c
@@ -50,6 +50,8 @@ static void string_into_line_array(char *buffer, struct source_file_t *struct_pt
         else
             iterator++;
     }
+
+    struct_ptr->line_count++;
 }
 
 struct source_file_t * file_to_struct(char *path)
